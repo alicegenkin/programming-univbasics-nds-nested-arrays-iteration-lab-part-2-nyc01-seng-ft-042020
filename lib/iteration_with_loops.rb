1708ae6e08693a127bbabcb54c2fa1550453914a
Alice <agenkin1@gmail.com>
2020-04-06 16:28:34 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,14 +3,13 @@ def find_min_in_nested_arrays(array)
   array.each do |inner_array|
     min = inner_array[0]
     inner_array.each do |inner_element|
-  
+
       if inner_element < min
         min = inner_element
-        
+
       end
     end
     newArray.push(min)
   end
   return newArray
 end
-
